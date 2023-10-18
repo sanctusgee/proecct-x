38aedf135a34694a31d279b0e0b29a01a0c0356e
sanctusgee <godwin.dsg@gmail.com>
2023-10-17 19:10:00 -0600

    added run to app.py

--- app.py
+++ app.py
@@ -19,6 +19,6 @@ def echo_input():
     return "You entered: " + input_text
 
 
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
+# if __name__ == "__main__":
+  #  port = int(os.environ.get("PORT", 5000))
+   # app.run(host="0.0.0.0", port=port)
