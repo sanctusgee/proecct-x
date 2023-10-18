8968c113eb07dc216bb673a40d1b4756996c7c19
sanctusgee <godwin.dsg@gmail.com>
2023-10-17 19:34:32 -0600

    added templates

--- app.py
+++ app.py
@@ -1,24 +1,10 @@
-#!/usr/bin/env python3
-
-from flask import Flask, request
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def main():
-    return '''
-     <form action="/echo_user_input" method="POST">
-         <input name="user_input">
-         <input type="submit" value="Submit!">
-     </form>
-     '''
-
-@app.route("/echo_user_input", methods=["POST"])
-def echo_input():
-    input_text = request.form.get("user_input", "")
-    return "You entered: " + input_text
-
+    return render_template("index.html")
 
 if __name__ == "__main__":
-  port = int(os.environ.get("PORT", 5000))
-  app.run(host="0.0.0.0", port=port)
+    app.run(debug=True)
